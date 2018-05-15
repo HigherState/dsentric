f3cee60dee69bc17f8facda1c6d4c5dd2a3f4141
jdrphillips <james.phillips@sclgroup.cc>
2018-05-15 09:26:45 +0100

    Add isNull to Data
    There are many configurations of data which represent null:
    
    - null
    - DNull
    - Data(null)
    - Data(DNull)
    
    etc
    
    The isNull method makes it easier to check for null data.

---
+++ maps/src/test/scala/dsentricTests/IsNullTests.scala
@@ -0,0 +1,28 @@
+package dsentricTests
+
+import dsentric._
+import org.scalatest.{FunSuite, Matchers}
+
+class IsNullTests extends FunSuite with Matchers {
+  import Dsentric._
+  import PessimisticCodecs._
+
+  test("Data.isNull on non-null data") {
+    Data(5).isNull should be (false)
+    DObject.empty.isNull should be (false)
+    DObject(Map("data" -> Data(5))).isNull should be (false)
+    DArray.empty.isNull should be (false)
+    new DArray(Vector(null)).isNull should be (false)
+    new DFalse().isNull should be (false)
+    new DTrue().isNull should be (false)
+  }
+
+  test("Data.isNull on null data") {
+    dNull.isNull should be (true)
+    Data(dNull).isNull should be (true)
+    Data(Data(dNull)).isNull should be (true)
+    new Data { val value: Any = null }.isNull should be (true)
+    new DArray(null).isNull should be (true)
+  }
+
+}
