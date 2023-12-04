9b78cd732ba5bfaeb08d148ea64114ca03c1211c
Jamie Pullar <jamie.pullar@orgvue.com>
2023-12-04 13:14:43 +0000

    Removed apply on matcher

--- maps/src/main/scala/dsentric/Matcher.scala
+++ maps/src/main/scala/dsentric/Matcher.scala
@@ -1,32 +1,19 @@
 package dsentric
 
-import dsentric.codecs.DCodec
-
-sealed trait Matcher  {
-  def isTypeMatch(j:Raw):Boolean
-  protected def default:Any
+sealed trait Matcher {
+  def apply(j: Raw): Boolean
+  protected def default: Any
 }
 
 object ExistenceMatcher extends Matcher {
-  def isTypeMatch(j:Raw):Boolean = true
-  protected def default:Any = DNull
+  def apply(j: Raw): Boolean = true
+  protected def default: Any = DNull
 
 }
 
-final case class ValueMatcher[T](value:T)(implicit _codec:DCodec[T]) extends Matcher {
-  val default: Raw = _codec(value)
-  def isTypeMatch(j: Raw): Boolean = j == default
-}
-
-trait DataMatchers {
-  implicit def valueMatcher[T](value:T)(implicit _codec:DCodec[T]): ValueMatcher[T] =
-    ValueMatcher(value)
-}
-
-class MatcherUnapply private[dsentric](key: String, matcher:Matcher) extends ApplicativeMatcher[DObject] {
-  def unapply(j:DObject):Boolean = {
+class MatcherUnapply private[dsentric] (key: String, matcher: Matcher) extends ApplicativeMatcher[DObject] {
+  def unapply(j: DObject): Boolean =
     j.value
       .get(key)
-      .fold(false) { v => matcher.isTypeMatch(v) }
-  }
+      .fold(false)(v => matcher(v))
 }
